121594680a1bd8a2fd916b43686d5ee41aa770c0
danrae pray <danrae.pray@outlook.com>
2023-04-16 15:47:49 -0500

    we now have a package gun muddafudda!

---
+++ Assets/MyGame/MyPackageGun.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPackageGun : MonoBehaviour
+{
+    public GameObject packagePrefab;
+
+    public float forceStrength = 100.0f;
+
+    public Rigidbody kartRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var package = GameObject.Instantiate(packagePrefab);
+            package.transform.position = this.transform.position;
+
+            var packageRB = package.GetComponent<Rigidbody>();
+            packageRB.velocity = kartRigidbody.velocity;
+
+            var force = (this.transform.forward + this.transform.up) * forceStrength;
+
+            packageRB.AddRelativeForce(force, ForceMode.Force);
+        }
+    }
+}
